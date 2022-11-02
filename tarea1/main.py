37b4902802956f89a96b6f546cffeab0b05a069b
renato <renatocespedesf@gmail.com>
2022-11-02 08:15:05 -0500

    archivo tarea1

---
+++ tarea1/main.py
@@ -0,0 +1,16 @@
+import csv
+
+
+
+class Libro:
+    def __init__(self):
+        
+        self.__id=0
+        self.__titulo=""
+        self.__genero=""
+        self.__id_ISBN=""
+        self.__editorial=""
+        self.__autores=[]
+    
+    #SETTER
+   
